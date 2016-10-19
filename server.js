772b52572ceb641f41b958bfc424f5e849123a30
丁一鸣 <315129552@qq.com>
2016-10-19 12:05:28 +0800

    update

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var AV = require('leanengine');
 var path = require('path');
 var app = express();
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || 'HX00MPYKnp7MGz1q1FeBUKiC-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'KtfTMPVfYczU2Nl6Wwmp6tdQ',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'h37P7iioJtS2eVb3UUvwD0TF'
+});
+
 app.use(AV.express());
 app.use(express.static('./'));
 app.get('/', function(req, res) {
